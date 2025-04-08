cc8a93abde2e66b1fd0dbaea190930ba5c87fa45
Borsch <l.boristag@gmail.com>
2025-04-08 03:26:41 +0300

    parsing env variable added

--- framework/config.py
+++ framework/config.py
@@ -1,15 +1,23 @@
+import os
+from dotenv import load_dotenv
 import configparser
 from configparser import ConfigParser
 
+DEFAULT_CONFIG_PATH = "/var/opt/kaspersky/config.ini"
+
 
 def read_config(config_path: str | None = None) -> ConfigParser:
-    config_path = config_path if config_path is not None else "/var/opt/kaspersky/config.ini"
+    if config_path is None:
+        load_dotenv()
+        config_path = os.getenv("CONFIG_PATH", DEFAULT_CONFIG_PATH)
+
     config = configparser.ConfigParser()
     config.read(config_path)
     return config
 
 
 if __name__ == "__main__":
-    conf = read_config(r"E:\pets\KasperskyValidate\config\config.ini")
+    print(os.getenv("CONFIG_PATH"))
+    conf = read_config()
     print(type(conf))
     print(conf["General"])
--- tests/test_config_validation.py
+++ tests/test_config_validation.py
@@ -1,1 +1,12 @@
 import pytest
+
+from framework.config import read_config
+
+
+@pytest.fixture
+def valid_config():
+    return read_config(r"E:\pets\KasperskyValidate\config\config.ini")
+
+def test_required_sections(valid_config):
+    assert "General" in valid_config
+    assert "Watchdog" in valid_config
