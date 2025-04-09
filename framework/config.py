f8465cafbf62cd01ac53d2404e6e7ee525bee071
Borsch <l.boristag@gmail.com>
2025-04-09 22:01:48 +0300

    virtual env variable

--- framework/config.py
+++ framework/config.py
@@ -9,7 +9,7 @@ DEFAULT_CONFIG_PATH = "/var/opt/kaspersky/config.ini"
 
 def read_config(config_path: str | None = None) -> ConfigParser:
     if config_path is None:
-        load_dotenv()
+        # load_dotenv()
         config_path = os.getenv("CONFIG_PATH", DEFAULT_CONFIG_PATH)
         # config_path = str(Path(config_path).resolve())
 
